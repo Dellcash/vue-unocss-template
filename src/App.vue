d069a32ee83ab7375e103e9dee62477b5dd09721
unknown <omiddellcash@gmail.com>
2022-07-10 06:53:18 +0430

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterView />
+</template>
+
+<style>
+@font-face {
+  font-family: "IRANSans";
+  src: url("./fonts/FontsFree-Net-ir_sans.ttf");
+}
+
+* {
+  font-family: "IRANSans";
+}
+</style>
