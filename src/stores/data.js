a3ee6c186a54f1a3b1e00062f1764a698967476d
Dellcash <77158396+Dellcash@users.noreply.github.com>
2022-09-22 08:53:27 +0330

    Update and rename counter.js to data.js

---
+++ src/stores/data.js
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+
+export const useDataStore = defineStore({
+  id: 'data',
+  state: () => ({
+    status: 'Good'
+  }),
+  getters: {
+  },
+  actions: {
+  }
+})
